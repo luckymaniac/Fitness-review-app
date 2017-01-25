ef50ef2c0403b620838f380230772a0dd2236ffd
luckymaniac <suzuki.saburo16@gmail.com>
2017-01-26 01:09:08 +0900

    update client-info

--- scripts/components/client-info/client-info.controller.js
+++ scripts/components/client-info/client-info.controller.js
@@ -1,6 +1,8 @@
 class ClientInfoController {
-  constructor(Goal) {
+  constructor(Goal, Client, AlertService) {
     this._Goal = Goal;
+    this._Client = Client;
+    this._AlertService = AlertService;
   }
 
   $onInit() {
@@ -16,8 +18,9 @@ class ClientInfoController {
 
     const goal_id = _.get(this.client, 'bio.goal_id');
     if (goal_id) {
-      this._Goal.get(goal_id).then(res => {
-        this.goal = res.goal;
+      this._Goal.list().then(res => {
+        this.goals = res.goals;
+        this.goal = _.find(this.goals, {id: goal_id});
       });
     }
   }
@@ -25,8 +28,34 @@ class ClientInfoController {
   toggle() {
     this.isExpanded = !this.isExpanded;
   }
+
+  editGoal(v = true) {
+    this.isEditGoal = v;
+  }
+
+  update() {
+    const data = {
+      goal_id: this.goal.id
+    };
+    this._Client.update(this.client.id, data)
+      .then(res => {
+        if (res && res.success) {
+          this.client = res.client;
+          this._AlertService.success("Client's goal is updated!");
+          this.editGoal(false);
+        } else {
+          this._AlertService.error("Failed to update Client's goal");
+        }
+      });
+  }
+
+  cancel() {
+    const goal_id = _.get(this.client, 'bio.goal_id');
+    this.goal = _.find(this.goals, {id: goal_id});
+    this.editGoal(false);
+  }
 }
 
-ClientInfoController.$inject = ['Goal'];
+ClientInfoController.$inject = ['Goal', 'Client', 'AlertService'];
 
 export default ClientInfoController;
