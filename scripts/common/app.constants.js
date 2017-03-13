e876841a9c8234e5aca019695d322a8c6bb2e658
luckymaniac <suzuki.saburo16@gmail.com>
2017-03-13 09:07:31 +0800

    update server ip

--- scripts/common/app.constants.js
+++ scripts/common/app.constants.js
@@ -1,7 +1,7 @@
 const AppConstants = {
-  api: 'http://ec2-35-161-43-243.us-west-2.compute.amazonaws.com/api/coaches',
+  api: 'http://35.163.77.181/api/coaches',
   appName: 'Eat To Perform',
-  appKey: '35-161-43-243',
+  appKey: '35.163.77.181.coach',
   trend_max_data: {
     carbs: 600,
     fat: 200,
