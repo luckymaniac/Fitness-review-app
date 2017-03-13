7e94d72b72a05a89bdddf99e536c4d17f7c1bd94
luckymaniac <suzuki.saburo16@gmail.com>
2017-03-13 11:21:12 +0800

    update coach app: reviews page, client_info component

--- scripts/pages/reviews/reviews.controller.js
+++ scripts/pages/reviews/reviews.controller.js
@@ -1,8 +1,9 @@
 class ReviewsController {
-  constructor(MacroReview, Auth) {
+  constructor(MacroReview, Auth, Goal) {
     window._$ctrl = this;
     this._MacroReview = MacroReview;
     this._Auth = Auth;
+    this._Goal = Goal;
 
     this.init();
   }
@@ -16,6 +17,8 @@ class ReviewsController {
     };
     this.total = 0;
 
+    this.goals = [];
+
     this.load();
   }
 
@@ -27,6 +30,11 @@ class ReviewsController {
         this.macro_reviews = res.macro_reviews;
       }
     });
+
+    this._Goal.list()
+      .then(res => {
+        this.goals = res.goals;
+      });
   }
 
   onSearch() {
@@ -51,8 +59,25 @@ class ReviewsController {
   isSuper() {
     return this._Auth.me.is_super;
   }
+
+  showGoal(client) {
+    let result = '';
+
+    const goal_id = _.get(client, 'bio.goal_id', null);
+    if (goal_id !== null) {
+      const goal = _.find(this.goals, {id: goal_id});
+      if (goal) {
+        result = goal.description.toUpperCase();
+        if (goal.phases) {
+          const phase = _.get(client, 'bio.goal_phase') || 1;
+          result += ' - Phase ' + phase;
+        }
+      }
+    }
+    return result;
+  }
 }
 
-ReviewsController.$inject = ['MacroReview', 'Auth'];
+ReviewsController.$inject = ['MacroReview', 'Auth', 'Goal'];
 
 export default ReviewsController;
